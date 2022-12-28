2095af0357c70221a955a489684ca6360ac04a7a
tanvinh152 <120997767+ltvinh1502@users.noreply.github.com>
2022-12-28 09:37:54 +0700

    fix-training-1

--- Training1/airbnb-clone/src/components/Category/index.js
+++ Training1/airbnb-clone/src/components/Category/index.js
@@ -1,13 +1,16 @@
 import { Image } from "antd";
-import React from "react";
+import React, { memo } from "react";
 
-const Category = ({ id, title, imageUrl }) => {
-    return (
-        <div className="flex flex-col py-4 items-center hover:opacity-40 cursor-pointer hover:border-b-2">
-            <img alt={imageUrl} src={imageUrl} width={24} height={24} />
-            <span className="text-sm whitespace-nowrap">{title}</span>
-        </div>
-    );
+const Category = ({ id, title, imageUrl, handleDeleteItem }) => {
+  return (
+    <div
+      onClick={() => handleDeleteItem(id)}
+      className="flex flex-col py-4 items-center hover:opacity-40 cursor-pointer hover:border-b-2"
+    >
+      <img alt={imageUrl} src={imageUrl} width={24} height={24} />
+      <span className="text-sm whitespace-nowrap">{title}</span>
+    </div>
+  );
 };
 
-export default Category;
+export default memo(Category);
